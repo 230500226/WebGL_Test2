f959fd67a9e3eed0fc3646d67bafc7aead286bde
Shahied Rustin <97807830+snowyru@users.noreply.github.com>
2024-06-02 01:58:27 +0200

    Q3

--- Q2.js
+++ Q2.js
@@ -151,7 +151,7 @@ function main(){
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    //set the texure picture here by id
+    //set the texure picture here by id see the index.html
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, document.getElementById('pictureID'));
 
     gl.useProgram(program);
